a3023d6b717444ef3ef29ec70df623b77d188015
Stefan Portay <stefanportay@gmail.com>
2013-07-25 17:27:25 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinup::Application.config.secret_token = 'b015d01d5abedd37e2b1ac10c9f8ccb40185d4767f2bfe4dbb0c9fb70dd3d4753c6118acaf6a80870111d3304b8edc1c25c26133a7d62395325d30e90d6951e9'
